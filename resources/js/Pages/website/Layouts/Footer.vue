fce3da50ec65e3204de1291def9cd1df80fea34e
MahdiSohaily <mahdi.sohaily4030@gmail.com>
2023-06-28 09:19:21 +0330

    Add new footer links

--- resources/js/Pages/website/Layouts/Footer.vue
+++ resources/js/Pages/website/Layouts/Footer.vue
@@ -5,114 +5,6 @@ const props = defineProps({
 });
 </script>
 <template>
-  <!-- <footer class="footer pt-5 mt-5">
-    <hr class="horizontal dark mb-5">
-    <div class="container">
-      <div class=" row">
-        <div class="col-md-3 mb-4 ms-auto">
-          <div>
-            <h6 class="text-gradient text-primary font-weight-bolder">Company Name</h6>
-          </div>
-          <div>
-            <h6 class="mt-3 mb-2 opacity-8">Social</h6>
-            <ul class="d-flex flex-row ms-n3 nav">
-              <li class="nav-item">
-                <Link class="nav-link pe-1" href="https://www.facebook.com/#/" target="_blank">
-                  <i class="fab fa-facebook text-lg opacity-8"></i>
-                </Link>
-              </li>
-
-              <li class="nav-item">
-                <a class="nav-link pe-1" href="https://twitter.com/#" target="_blank">
-                  <i class="fab fa-twitter text-lg opacity-8"></i>
-                </a>
-              </li>
-
-              <li class="nav-item">
-                <a class="nav-link pe-1" href="https://instagram.com/#" target="_blank">
-                  <i class="fab fa-instagram text-lg opacity-8"></i>
-                </a>
-              </li>
-
-
-              <li class="nav-item">
-                <a class="nav-link pe-1" href="https://github.com/#official" target="_blank">
-                  <i class="fab fa-linkedin text-lg opacity-8"></i>
-                </a>
-              </li>
-
-              <li class="nav-item">
-                <a class="nav-link pe-1" href="https://www.youtube.com/channel#" target="_blank">
-                  <i class="fab fa-youtube text-lg opacity-8"></i>
-                </a>
-              </li>
-            </ul>
-          </div>
-        </div>
-        <div class="col-md-4 col-sm-6 col-6 mb-4">
-          <div>
-            <h6 class="text-gradient text-primary text-sm">Company</h6>
-            <ul class="flex-column ms-n3 nav">
-              <li class="nav-item">
-                <Link class="nav-link" :href="route('about')">
-                Über uns
-                </Link>
-              </li>
-
-              <li class="nav-item">
-                <Link class="nav-link" :href="route('services')">
-                  Dienstleistungen
-                </Link>
-              </li>
-
-              <li class="nav-item">
-                <Link class="nav-link" :href="route('about')">
-                  What we have done
-                </Link>
-              </li>
-
-              <li class="nav-item">
-                <Link class="nav-link" :href="route('blog')">
-                  Blog
-                </Link>
-              </li>
-            </ul>
-          </div>
-        </div>
-        <div class="col-md-4 col-sm-6 col-6 mb-4">
-          <div>
-            <h6 class="text-gradient text-primary text-sm">Hilfe Unterstützung</h6>
-            <ul class="flex-column ms-n3 nav">
-              <li class="nav-item">
-                <a class="nav-link" href="https://contact-us" target="_blank">
-                  Kontaktiere uns
-                </a>
-              </li>
-
-              <li class="nav-item">
-                <a class="nav-link" href="https://knowledge-center" target="_blank">
-                  Frequently Asked
-                </a>
-              </li>
-
-              <li class="nav-item">
-                <a class="nav-link" href="https://services.creative-tim.com/?ref=ct-soft-ui-footer" target="_blank">
-                  Custom Development
-                </a>
-              </li>
-
-              <li class="nav-item">
-                <a class="nav-link" href="https://sponsorships" target="_blank">
-                  Sponsorships
-                </a>
-              </li>
-
-                </ul>
-              </div>
-            </div>
-                      </div>
-                  </div>
-                </footer> -->
   <footer class="footer py-5 bg-gradient-dark position-relative overflow-hidden">
     <img src="../../../../../public/assets/img/shapes/waves-white.svg" alt="pattern-lines"
       class="position-absolute start-0 top-0 w-100 opacity-6">
@@ -139,6 +31,16 @@ const props = defineProps({
             <li class="nav-item">
               <Link class="nav-link text-white opacity-8" :href="route('home')" target="_blank">
               Kontaktiere uns
+              </Link>
+            </li>
+            <li class="nav-item">
+              <Link class="nav-link text-white opacity-8" :href="route('home')" target="_blank">
+              Privecy Policy
+              </Link>
+            </li>
+            <li class="nav-item">
+              <Link class="nav-link text-white opacity-8" :href="route('home')" target="_blank">
+              Terms & Conditions
               </Link>
             </li>
           </ul>
