5c90b5b49a278bfb493af51b1c099aca99f4c025
MahdiSohaily <mahdi.sohaily4030@gmail.com>
2023-07-05 13:52:07 +0330

    Add script to get all the frequently asked questions

--- resources/js/Pages/FAQ/Show.vue
+++ resources/js/Pages/FAQ/Show.vue
@@ -18,7 +18,7 @@ const props = defineProps({
                 <div class="card-header pb-0">
                     <h6>All the frequently asked questions</h6>
                 </div>
-                <div class="card-body px-0 pt-0 pb-2">
+                <div class="card-body px-4 pt-0 pb-2">
                     <div class="table-responsive p-0">
                         <table class="table align-items-center mb-0">
                             <thead>
@@ -26,22 +26,15 @@ const props = defineProps({
                                     <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 ps-2">
                                         Question
                                     </th>
-                                    <th
-                                        class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">
-                                        Answer
+                                    <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 ps-2">
+                                        Operation
                                     </th>
-                                    <th
-                                        class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">
-                                        Operation</th>
                                 </tr>
                             </thead>
                             <tbody>
-                                <tr>
+                                <tr v-for="faq of faqs">
                                     <td>
-                                        <p class="text-xs text-secondary mb-0">john@creative-tim.com</p>
-                                    </td>
-                                    <td>
-                                        <p class="text-xs text-secondary mb-0">Organization</p>
+                                        <p class="text-xs text-secondary mb-0">{{ faq.question }}</p>
                                     </td>
                                     <td>
                                         <a href="javascript:;" class="text-secondary font-weight-bold text-xs"
@@ -58,4 +51,8 @@ const props = defineProps({
         </div>
     </AppLayout>
 </template>
-<style></style>
+<style>
+.w-44 {
+    width: 300px !important;
+}
+</style>
